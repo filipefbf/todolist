4f1bb1dd93338de81e0e10a7e936cf98302d427a
Filipe <filipe_fbf@hotmail.com>
2024-05-27 22:19:58 -0300

    feat: creating controller

---
+++ src/main/java/br/com/desafio/todolist/controller/TodoController.java
@@ -0,0 +1,42 @@
+package br.com.desafio.todolist.controller;
+
+import br.com.desafio.todolist.entity.Todo;
+import br.com.desafio.todolist.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    //create
+    @PostMapping
+    public List<Todo> create(@RequestBody Todo todo) {
+        return todoService.create(todo);
+    }
+
+    //list
+    @GetMapping
+    public List<Todo> list() {
+        return todoService.list();
+    }
+
+    //update
+    @PutMapping
+    public List<Todo> update(@RequestBody Todo todo) {
+        return todoService.update(todo);
+    }
+
+    //delete
+    @DeleteMapping("/{id}")
+    public List<Todo> delete(@PathVariable("id") Long id){
+        return todoService.delete(id);
+    }
+}
--- src/main/java/br/com/desafio/todolist/service/TodoService.java
+++ src/main/java/br/com/desafio/todolist/service/TodoService.java
@@ -23,7 +23,7 @@ public class TodoService {
 
     public List<Todo> list() {
         Sort sort = Sort.by("prioridade").descending().and(Sort.by("nome").ascending());
-        todoRepository.findAll(sort);
+        return todoRepository.findAll(sort);
     }
 
     public List<Todo> update(Todo todo) {
