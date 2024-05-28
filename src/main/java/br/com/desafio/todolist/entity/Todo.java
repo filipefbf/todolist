a13ec54f7c2fb2141c517ff2a3dfd7131e0dccab
Filipe <filipe_fbf@hotmail.com>
2024-05-27 22:03:01 -0300

    feat: create todo entity

---
+++ src/main/java/br/com/desafio/todolist/entity/Todo.java
@@ -0,0 +1,22 @@
+package br.com.desafio.todolist.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private boolean done;
+
+}
